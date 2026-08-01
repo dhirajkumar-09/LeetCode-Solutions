5fe8fdfa0bccceb6b2f90394f49b377b80882e6a
Dhiraj Kumar <dhidna9090@gmail.com>
2026-08-01 16:44:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.78%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.py
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.py
@@ -5,9 +5,7 @@ class Solution(object):
         :type original: int
         :rtype: int
         """
-        if original not in nums:
-            return original 
-        nums_list=sorted(set(nums))
+        nums_list=set(nums)
         while original in nums_list:
             original*=2
         return original

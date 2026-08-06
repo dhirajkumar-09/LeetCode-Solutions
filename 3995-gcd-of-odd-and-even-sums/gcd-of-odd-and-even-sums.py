38d4bb5375f6f4cad01360b5a8e8d1fbfbc96e08
Dhiraj Kumar <dhidna9090@gmail.com>
2026-08-06 08:49:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (21.99%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
@@ -4,14 +4,4 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        import fractions
-        le=2*n+1
-        lo=2*n
-        se=0
-        so=0
-        for i in range(2,le,2):
-            se+=1
-        for i in range(1,lo,2):
-            so+=1
-        result=fractions.gcd(se,so)
-        return result
+        return n
